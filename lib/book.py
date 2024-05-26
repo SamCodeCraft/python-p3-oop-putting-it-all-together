886c7f6be8297ece634f030c37d24aadf0f45d18
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-05-26 11:20:43 +0300

    OOP programming Lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,20 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None  # Initialize the internal page count attribute
+        self.page_count = page_count  # Use the property setter to validate the initial value
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
